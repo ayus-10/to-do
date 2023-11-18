ad63b00c53e94bedf26e86856d63690c6fef93a9
Aayush <136055230+ayus-10@users.noreply.github.com>
2023-11-18 15:31:58 +0545

    made todo-item a form for enter key functionality

--- assets/app.js
+++ assets/app.js
@@ -1,64 +1,64 @@
-function addToDoItem() {
-    // To get the input value for todo-item
-    var todoTextElement = document.getElementById("todoText");
-    var todoText = todoTextElement.value;
+function addToDoItem(event) {
+  // Prevent page refresh
+  event.preventDefault();
 
-    if (todoText == '')
-        return;
+  // To get the input value for todo-item
+  var todoTextElement = document.getElementById("todoText");
+  var todoText = todoTextElement.value;
 
-    // Create a new todo-item and add required content
-    var toDoItem = document.createElement("div");
-    toDoItem.classList.add("todo-item");
-    const toDoItemContents =
-        `<input type="checkbox" class="todo-item-checkbox" onclick="crossToDoItem(event)">
-        <div type="text" class="todo-item-text" placeholder="Click to add a note">${todoText}</div>
-        <button class="fa fa-close close-btn" onclick="removeToDoItem(event)"></button>`;
-    toDoItem.innerHTML = toDoItemContents;
+  if (todoText == "") return;
 
-    // Append the created todo-item element to parent element
-    const toDosContainer = document.getElementsByClassName("todos-container")[0];
-    toDosContainer.append(toDoItem);
+  // Create a new todo-item and add required content
+  var toDoItem = document.createElement("div");
+  toDoItem.classList.add("todo-item");
+  const toDoItemContents = `
+    <input type="checkbox" class="todo-item-checkbox" onclick="crossToDoItem(event)">
+    <div type="text" class="todo-item-text" placeholder="Click to add a note">${todoText}</div>
+    <button class="fa fa-close close-btn" onclick="removeToDoItem(event)"></button>`;
+  toDoItem.innerHTML = toDoItemContents;
 
-    // Make the input field empty for next use
-    todoTextElement.value = '';
+  // Append the created todo-item element to parent element
+  const toDosContainer = document.getElementsByClassName("todos-container")[0];
+  toDosContainer.append(toDoItem);
 
-    // Save the todos in local storage
-    saveTodos();
+  // Make the input field empty for next use
+  todoTextElement.value = "";
+
+  // Save the todos in local storage
+  saveTodos();
 }
 
 function removeToDoItem(event) {
-    // Get the close-btn that has been clicked and remove its parent element
-    var button = event.target;
-    var toDoItem = button.parentElement;
-    toDoItem.remove();
+  // Get the close-btn that has been clicked and remove its parent element
+  var button = event.target;
+  var toDoItem = button.parentElement;
+  toDoItem.remove();
 
-    // Update the todos in local storage
-    saveTodos();
+  // Update the todos in local storage
+  saveTodos();
 }
 
 function crossToDoItem(event) {
-    // Get the clicked checkbox and cross/uncross the corresponding todo text
-    var checkbox = event.target;
-    var todoItem = checkbox.parentElement;
-    var todoText = todoItem.getElementsByClassName("todo-item-text")[0];
-    todoText.classList.toggle("crossed");
+  // Get the clicked checkbox and cross/uncross the corresponding todo text
+  var checkbox = event.target;
+  var todoItem = checkbox.parentElement;
+  var todoText = todoItem.getElementsByClassName("todo-item-text")[0];
+  todoText.classList.toggle("crossed");
 
-    // Add the attribute checked if the checkbox is checked, so that the checked state of checkbox can be stored in local storage
-    if (checkbox.checked)
-        checkbox.setAttribute('checked', 'checked');
-    else
-        checkbox.removeAttribute('checked');
+  // Add the attribute checked if the checkbox is checked, so that the checked state of checkbox can be stored in local storage
+  if (checkbox.checked) checkbox.setAttribute("checked", "checked");
+  else checkbox.removeAttribute("checked");
 
-    // Update the todos in local storage
-    saveTodos();
+  // Update the todos in local storage
+  saveTodos();
 }
 
 function saveTodos() {
-    localStorage.setItem("todos", toDos.innerHTML);
+  localStorage.setItem("todos", toDos.innerHTML);
 }
 
 function retrieveTodos() {
-    toDos.innerHTML = localStorage.getItem("todos");
+  toDos.innerHTML = localStorage.getItem("todos");
 }
 
 // Get all created todo-item
